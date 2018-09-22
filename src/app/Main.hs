395f2ae4acfcf2f055214a63ade360d6102e7c64
jinilover <columbawong@gmail.com>
2018-09-22 16:42:29 +1000

    add more `POST` examples

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -6,6 +6,7 @@ module Main where
 import Servant
 import Network.Wai.Handler.Warp
 import Data.Time.Calendar
+import Control.Exception.Base
 import Control.Monad.IO.Class
 import Control.Monad.Error.Class
 import GHC.IO.Exception
@@ -21,6 +22,10 @@ main :: IO ()
 main = do
   AppConfig{..} <- loadAppConfig
   Db.migrateDb dbscriptsDir
+  try $ Db.addUser User { name = "Isaac Newton"
+                  , age = 372
+                  , email = "isaac@newton.co.uk"
+                  , registrationDate = "1683-03-01"}
   run appPort app
 
 server :: Server ServiceAPI
